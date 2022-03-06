e6f2b184acfbe8b5281106e0e742793681266257
hp Elite 8300 <gouchereyves@gmail.com>
2022-03-06 13:41:57 +0100

    initial commit

---
+++ src/test/java/com/erp/lymytz/BackEndErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.lymytz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
